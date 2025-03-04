aeb43ef41244cffcede550a1a086e8db0f37d9ad
Olya Vorontsova <16olya1996@gmail.com>
2025-03-04 21:44:40 +0100

    save data in localestorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
@@ -10,28 +10,32 @@ import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
 export default function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
   });
 
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback = Math.round((feedback.good / totalFeedback) * 100);
+
   const resetFeedback = () => {
-    setFeedback({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
+    const resetFeeadback = { good: 0, neutral: 0, bad: 0 };
+    setFeedback(resetFeeadback);
+    localStorage.setItem("feedback", JSON.stringify(resetFeeadback));
   };
 
   const updateFeedback = (feedbackType) => {
-    console.log("Feedback Type:", feedbackType);
     setFeedback((prevFeedback) => ({
       ...prevFeedback,
       [feedbackType]: prevFeedback[feedbackType] + 1,
     }));
   };
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
 
   return (
     <div>
@@ -50,6 +54,7 @@ export default function App() {
           neutral={feedback.neutral}
           bad={feedback.bad}
           total={totalFeedback}
+          positive={positiveFeedback}
         />
       ) : (
         <Notification />
